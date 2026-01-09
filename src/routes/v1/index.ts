e832fd66b03f2f936d95c7ed8949b1f6767a6885
Heyimlulu <puffycatwhiskers@gmail.com>
2026-01-09 14:49:42 +0100

    Temporaliy disabled trendings route for Google API changes

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -1,11 +1,11 @@
 import { Router} from "express";
 
 import queriesRoute from "./queries";
-import trendingsRoute from "./trendings";
+//import trendingsRoute from "./trendings";
 
 const v1Route = Router();
 
 v1Route.use("/", queriesRoute);
-v1Route.use("/", trendingsRoute);
+//v1Route.use("/", trendingsRoute);
 
 export default v1Route;
